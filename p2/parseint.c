4b275e529695875d793da7bc00c53e7b3ca27778
Brybbinnn <bryndisgunnarsd@gmail.com>
2024-01-17 14:20:13 +0000

    damn dawg :dog:

--- p2/parseint.c
+++ p2/parseint.c
@@ -1,13 +1,19 @@
 #include "parseint.h"
+#include <stdio.h>
 
 /*
  * Returns the value of c or -1 on error
  */
 int convertDecimalDigit(char c)
 {
-    (void)c;
-
-    return -1;
+    (void)c;                  
+    if (c >= '0' && c <= '9') {
+        // Convert char to integer and return
+        return c - '0';
+    } else {
+        // Not a valid decimal digit, return -1
+        return -1;
+    }
 }
 
 /*
@@ -19,4 +25,8 @@ int parseInt(char *string)
     (void)string;
 
     return -1;
+}
+
+int main(void) {
+   printf("%d", convertDecimalDigit('?'));
 }
