56c663b96c34cd9bb7be46ea158e750024b64e07
Viggó <viggoymir1@gmail.com>
2024-01-17 13:22:04 +0000

    :dragon: :fries:

--- print.c
+++ print.c
@@ -1,8 +1,16 @@
 #include "print.h"
+#include <stdio.h>
 
 void print_line(int64_t number, char *string)
 {
     (void) number;
     (void) string;
     // Add code here.
+    
+    printf("%d %s", number, string);
+}
+
+int main(void) {
+
+    print_line(42, "hello");
 }
