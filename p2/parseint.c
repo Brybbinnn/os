8e24907c0490fd284838db542522d6c6cb444073
Viggó <viggoymir1@gmail.com>
2024-01-19 12:10:11 +0000

    I Hate C

--- p2/parseint.c
+++ p2/parseint.c
@@ -16,44 +16,6 @@ int convertDecimalDigit(char c)
     }
 }
 
-/*
- * Parses a non-negative integer, interpreted as octal when starting with 0,
- * decimal otherwise. Returns -1 on error.
- */
-int parseInt(char *string) {
-    (void)string;
-
-    const char *p = string;
-
-    int string_array[4] = {};
-
-    if (p[0] == '0') {
-        printf("%g", "bitch if");
-        int octalToDecimal(int string);
-
-    } else if (p[0] != '0') {
-        printf("%g", "bitch else if");
-        for (int i = 0; i != '\0'; i++) {
-            string_array[i] = convertDecimalDigit(i);
-        }
-    }
-}
-
-//     int octal_sum = 0;
-
-//     if (string[0] == '0'){
-//         for (int i = 0; string[i] != '\0'; i++) {
-//             if (string[i] == '0'){
-//                 octal_sum += 0
-//             }
-//         }
-
-//     } else if (string[0] != '0') {
-//         printf("%d", string);
-//     } else {
-//         return -1;
-//     }
-
 int octalToDecimal(int n) {
     int decimalvalue = 0;
     int base = 1;
@@ -70,16 +32,40 @@ int octalToDecimal(int n) {
  
     return decimalvalue;
 }
- 
 
 
-int main(void) {
-    printf("%d", convertDecimalDigit('0'));
-    printf("%s", "The program finished running \"0\" through convertDecimalDigit. \n");
+/*
+ * Parses a non-negative integer, interpreted as octal when starting with 0,
+ * decimal otherwise. Returns -1 on error.
+ */
+int parseInt(char *string) {
+    (void)string;
+
+    int i, result = 0;
+    int ans;
+
+    for (int i = 0; string[i] != '\0'; i++) {
+        result = result * 10 + (string[i] - '0');
+    }
+
+    if (string[0] == '0') {
+        return octalToDecimal(result);
+    } else {
+        for (int i = 0; string[i] != '\0'; i++) {
+            ans += convertDecimalDigit(string[i]);
+        }
+        return ans;
+    }
+}
+
+
+int main() {
     printf("%d", parseInt("010"));
     printf("%s", "The program finished running \"010\" through parseInt. \n");
     printf("%d", parseInt("10"));
     printf("%s", "The program finished running \"10\" through parseInt. \n");
+    printf("%d", parseInt("350"));
+    printf("%s", "The program finished running \"350\" through parseInt. \n");
     printf("%d", parseInt("?"));
     printf("%s", "The program finished running \"?\" through parseInt. \n");
 }
