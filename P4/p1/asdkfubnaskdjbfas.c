11e11de1a7e13d25237de7677177a97f38785c52
Brybbinnn <bryndisgunnarsd@gmail.com>
2024-02-12 23:25:28 +0000

    :dragon: :eyes:

---
+++ P4/p1/asdkfubnaskdjbfas.c
@@ -0,0 +1,247 @@
+#include "scheduler.h"
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+typedef struct _QueueItem {
+    /*
+     * The tid of this item.
+     */
+    int tid;
+    /*
+     * The next item in the queue.
+     * NULL if there is no next item.
+     */
+    struct _QueueItem *next;
+} QueueItem;
+
+typedef struct _Queue {
+    /*
+     * The first item of the queue.
+     * NULL if the queue is empty.
+     */
+    QueueItem *head;
+    /*
+     * The last item of the queue.
+     * undefined if the queue is empty.
+     */
+    QueueItem *tail;
+
+    /*
+    My idea (and sample solution) is based on using one simple queue per priority level.
+    In this case these functions need to be extended.
+    As I said in the lecture, maybe by just one line
+    (or maybe two if you check for invalid priorities to not write out of bounds in an array with queue pointers).
+    */
+} Queue;
+
+typedef enum _ThreadState {
+    STATE_UNUSED = 0, // This entry in the _threads array is unused.
+
+    STATE_READY,      // The thread is ready and should be on a ready queue for
+                      //   selection by the scheduler
+    STATE_RUNNING,    // The thread is running and should not be on a ready queue
+    STATE_WAITING     // The thread is blocked and should not be on a ready queue
+} ThreadState;
+
+typedef struct _Thread {
+    int threadId;
+    ThreadState state;
+    /*
+     * Range: 0 ... MAX_PRIORITY (including)
+     * 0 is highest, MAX_PRIORITY is lowest priority
+     */
+    int priority;
+} Thread;
+
+Thread _threads[MAX_THREADS] = {{0}};
+
+/* TODO: Add global variables if needed. */
+Queue queue = {0};
+Queue priorityQueues[MAX_PRIORITY] = {{NULL, NULL}}; // Array of queues for each priority level //
+
+/*
+ * Adds a new, waiting thread.
+ */
+int startThread(int threadId, int priority)
+{
+    if (((threadId < 0) || (threadId >= MAX_THREADS) ||
+        (_threads[threadId].state != STATE_UNUSED)) ||
+        (priority < 0) || (priority > MAX_PRIORITY)) {
+
+        return -1;
+    }
+
+    _threads[threadId].threadId = threadId;
+    _threads[threadId].state    = STATE_WAITING;
+    _threads[threadId].priority = priority;
+    return 0;
+}
+
+/*
+ * Append to the tail of the queue.
+ * Does nothing on error.
+ */
+void _enqueue(Queue *queue, int tid)
+{
+    // Scheduler Solution >>
+
+    assert(queue != NULL);
+
+    QueueItem *item = (QueueItem*)malloc(sizeof(QueueItem));
+    if (item == NULL) {
+        return;
+    }
+
+    item->tid = tid;
+    item->next = NULL;
+    if (queue->head == NULL) {
+        queue->head = item;
+    } else {
+        assert(queue->tail != NULL);
+
+        queue->tail->next = item;
+    }
+
+    queue->tail = item;
+
+    // TODO: Implement
+}
+
+/*
+ * Remove and get the head of the queue.
+ * Return -1 if the queue is empty.
+ */
+int _dequeue(Queue *queue)
+{
+    // Scheduler Solution >>
+
+    assert(queue != NULL);
+
+    if (queue->head == NULL) {
+        return -1;
+    }
+
+    QueueItem *head = queue->head;
+    tid_t tid = head->tid;
+    queue->head = head->next;
+    free(head);
+
+    // TODO: Implement
+    return tid;
+}
+
+void initScheduler()
+{
+    // TODO: Implement if you need to initialize any global variables you added
+}
+
+static void _enqueueThread(tid_t threadId)
+{
+    assert((threadId >= 0) && (threadId < MAX_THREADS));
+    assert(_threads[threadId].state == STATE_READY);
+
+    // Append a thread to the right ready queue.
+    _enqueue(&priorityQueues, threadId);
+}
+
+/*
+ * Called whenever a waiting thread gets ready to run.
+ */
+void onThreadReady(int threadId)
+{
+    // Scheduler Solution >>
+
+    assert((threadId >= 0) && (threadId < MAX_THREADS));
+    assert(_threads[threadId].state == STATE_WAITING);
+
+    _threads[threadId].state = STATE_READY;
+    _enqueueThread(threadId); // þarf kannski að vera fyrir neðan assert
+
+    // TODO: Implement
+    //int priority = _threads[threadId].priority;
+    //assert(priority >= 0 && priority < MAX_PRIORITY); <---------------------------------------------------
+    /*
+    void onThreadReady(int threadId) is called if a thread in waiting state becomes
+    ready (e.g., thread was blocked on an I/O operation before, and the I/O operation
+    has finished). The thread needs to be placed in the appropriate runqueue.
+    */
+   // IMPORTANT ** APPROPRIATE RUNQUEUE **
+}
+
+/*
+ * Called whenever a running thread is forced of the CPU
+ * (e.g., through a timer interrupt).
+ */
+void onThreadPreempted(int threadId)
+{
+    // Scheduler Solution >>
+
+    assert((threadId >= 0) && (threadId < MAX_THREADS));
+    assert(_threads[threadId].state == STATE_RUNNING);
+
+    _threads[threadId].state = STATE_READY;
+    _enqueueThread(threadId);
+
+    // TODO: Implement
+    /*
+    void onThreadPreempted(int threadId) is called if a running thread was preempted.
+    It also needs to be placed in the right runqueue, as it is ready to continue.
+    */
+   // IMPORTANT ** RIGHT RUNQUEUE **
+}
+
+/*
+ * Called whenever a running thread needs to wait.
+ */
+void onThreadWaiting(int threadId)
+{
+    // Scheduler Solution >>
+
+    assert((threadId >= 0) && (threadId < MAX_THREADS));
+    assert(_threads[threadId].state == STATE_RUNNING);
+
+    _threads[threadId].state = STATE_WAITING;
+    // TODO: Implement
+    /*
+    void onThreadWaiting(int threadId) is called when a thread blocks (e.g., on an
+    I/O operation). Such a thread enters the waiting state and will not be part of any
+    runqueue.
+    */
+}
+
+/*
+ * Gets the id of the next thread to run and sets its state to running.
+ */
+int scheduleNextThread()
+{   for (int i = 0; i < MAX_PRIORITY; ++i) {
+        if (priorityQueues[i].head != NULL) {
+            return _dequeue(&priorityQueues[i]);
+        }
+    }
+    return -1;
+    // TODO: Implement
+    return -1;
+    /*
+    Implement the priority scheduling policy Your scheduling function should perform the
+    following basic operations whenever a new thread needs to be selected: 4 P-pt
+        • Find the ready queue with the highest priority that contains a ready thread
+        • Remove the first thread from the queue, updates its state and returns its thread id
+    */
+}
+
+
+#if 0
+int main() {
+	// Initially empty queue (head and tail is NULL)
+	Queue q = {NULL,NULL};
+
+	_enqueue( &q, 42 );
+	_enqueue( &q, 99 );
+	int x = _dequeue( &q );
+	printf("Expect: 42, and I got: %d\n", x);
+
+	x = _dequeue( &q );
+	printf("Expect: 99, and I got: %d\n", x);
+}
+#endif
--- P4/p1/scheduler.c
+++ P4/p1/scheduler.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <stdio.h>
 
+
 typedef struct _QueueItem {
     /*
      * The tid of this item.
@@ -49,23 +50,21 @@ typedef struct _Thread {
 
 Thread _threads[MAX_THREADS] = {{0}};
 
-/* TODO: Add global variables if needed. */
-Queue queue = {0};
+/* TODO: Add gloabl variables if needed*/
+int consec[MAX_PRIORITY]; // Consecutive counter for starvation
+Queue priorityQueues[MAX_PRIORITY] = {{NULL, NULL}}; // Array of queues for each priority level
 
 /*
  * Adds a new, waiting thread.
  */
-int startThread(int threadId, int priority)
-{
+int startThread(int threadId, int priority) {
     if (((threadId < 0) || (threadId >= MAX_THREADS) ||
         (_threads[threadId].state != STATE_UNUSED)) ||
-        (priority < 0) || (priority > MAX_PRIORITY)) {
-
+        (priority < 0) || (priority >= MAX_PRIORITY)) {
         return -1;
     }
-
     _threads[threadId].threadId = threadId;
-    _threads[threadId].state    = STATE_WAITING;
+    _threads[threadId].state = STATE_WAITING;
     _threads[threadId].priority = priority;
     return 0;
 }
@@ -74,39 +73,29 @@ int startThread(int threadId, int priority)
  * Append to the tail of the queue.
  * Does nothing on error.
  */
-void _enqueue(Queue *queue, int tid)
-{
-    // Scheduler Solution >>
+void _enqueue(Queue *queue, int tid) {
 
     assert(queue != NULL);
-
     QueueItem *item = (QueueItem*)malloc(sizeof(QueueItem));
+
     if (item == NULL) {
         return;
     }
 
     item->tid = tid;
     item->next = NULL;
+
     if (queue->head == NULL) {
         queue->head = item;
     } else {
         assert(queue->tail != NULL);
-
         queue->tail->next = item;
     }
-
+    
     queue->tail = item;
-
-    // TODO: Implement
 }
 
-/*
- * Remove and get the head of the queue.
- * Return -1 if the queue is empty.
- */
-int _dequeue(Queue *queue)
-{
-    // Scheduler Solution >>
+int _dequeue(Queue *queue) {
 
     assert(queue != NULL);
 
@@ -115,116 +104,105 @@ int _dequeue(Queue *queue)
     }
 
     QueueItem *head = queue->head;
-    tid_t tid = head->tid;
+    int tid = head->tid;
     queue->head = head->next;
     free(head);
-
-    // TODO: Implement
     return tid;
 }
 
-void initScheduler()
-{
-    // TODO: Implement if you need to initialize any global variables you added
+void initScheduler() {
+    // TODO: Implement if you need to initialize any global variables you add
+    for (int i = 0; i < MAX_PRIORITY; ++i) {
+        Queue queue = {0};
+        priorityQueues[i] = queue;
+    }
 }
 
-static void _enqueueThread(tid_t threadId)
-{
+static void _enqueueThread(int threadId) {
     assert((threadId >= 0) && (threadId < MAX_THREADS));
     assert(_threads[threadId].state == STATE_READY);
-
+    int priority = _threads[threadId].priority;
+    assert(priority >= 0 && priority < MAX_PRIORITY);
+    
     // Append a thread to the right ready queue.
-    _enqueue(&queue, threadId);
+    _enqueue(&priorityQueues[priority], threadId);
 }
 
 /*
  * Called whenever a waiting thread gets ready to run.
  */
-void onThreadReady(int threadId)
-{
-    // Scheduler Solution >>
-
+void onThreadReady(int threadId) {
     assert((threadId >= 0) && (threadId < MAX_THREADS));
     assert(_threads[threadId].state == STATE_WAITING);
 
     _threads[threadId].state = STATE_READY;
     _enqueueThread(threadId);
-
-    // TODO: Implement
-    /*
-    void onThreadReady(int threadId) is called if a thread in waiting state becomes
-    ready (e.g., thread was blocked on an I/O operation before, and the I/O operation
-    has finished). The thread needs to be placed in the appropriate runqueue.
-    */
 }
 
 /*
  * Called whenever a running thread is forced of the CPU
  * (e.g., through a timer interrupt).
  */
-void onThreadPreempted(int threadId)
-{
-    // Scheduler Solution >>
-
+void onThreadPreempted(int threadId) {
     assert((threadId >= 0) && (threadId < MAX_THREADS));
-    assert(_threads[threadId].state == STATE_RUNNING);
-
     _threads[threadId].state = STATE_READY;
     _enqueueThread(threadId);
-
-    // TODO: Implement
-    /*
-    void onThreadPreempted(int threadId) is called if a running thread was preempted.
-    It also needs to be placed in the right runqueue, as it is ready to continue.
-    */
 }
 
 /*
  * Called whenever a running thread needs to wait.
  */
-void onThreadWaiting(int threadId)
-{
-    // Scheduler Solution >>
-
+void onThreadWaiting(int threadId) {
     assert((threadId >= 0) && (threadId < MAX_THREADS));
-    assert(_threads[threadId].state == STATE_RUNNING);
-
     _threads[threadId].state = STATE_WAITING;
-    // TODO: Implement
-    /*
-    void onThreadWaiting(int threadId) is called when a thread blocks (e.g., on an
-    I/O operation). Such a thread enters the waiting state and will not be part of any
-    runqueue.
-    */
 }
-
 /*
  * Gets the id of the next thread to run and sets its state to running.
  */
 int scheduleNextThread()
 {
-    // TODO: Implement
-    return -1;
-    /*
-    Implement the priority scheduling policy Your scheduling function should perform the
-    following basic operations whenever a new thread needs to be selected: 4 P-pt
-        • Find the ready queue with the highest priority that contains a ready thread
-        • Remove the first thread from the queue, updates its state and returns its thread id
-    */
-}
+    int tid = 0;
 
+    // checks if the scheduler is starving low priority processes
+    for(int i = 0; i<MAX_PRIORITY; ++i){
+        if(priorityQueues[i].head != NULL){
+            if(consec[i] == 4){
+                consec[i] = 0;
+                continue; 
+            }
+
+            tid = _dequeue(&priorityQueues[i]);
+            consec[tid]++;
+            break;
+        }
+        else if(priorityQueues[i].head == NULL){
+            consec[i] = 0;
+        }
+        // If we've checked all priority queues without finding a suitable thread
+        // we reset the consec counter for each priority and return -1
+        if(i == MAX_PRIORITY-1){
+            for(int i = 0; i<MAX_PRIORITY; i++){
+                consec[i] = 0;
+            }
+            return -1;
+        }
+    }
+
+
+
+    _threads[tid].state = STATE_RUNNING;
+    return tid;
+}
 
 #if 0
-int main() {
-	// Initially empty queue (head and tail is NULL)
-	Queue q = {NULL,NULL};
-
-	_enqueue( &q, 42 );
-	_enqueue( &q, 99 );
-	int x = _dequeue( &q );
-	printf("Expect: 42, and I got: %d\n", x);
-
-	x = _dequeue( &q );
-	printf("Expect: 99, and I got: %d\n", x);
-}
+// int main() {
+//     // Initially empty queue (head and tail is NULL)
+//     Queue q = {NULL,NULL};
+//     _enqueue( &q, 42 );
+//     _enqueue( &q, 99 );
+//     int x = _dequeue( &q );
+//     printf("Expect: 42, and I got: %d\n", x);
+//     x = _dequeue( &q );
+//     printf("Expect: 99, and I got: %d\n", x);
+// }
 #endif
