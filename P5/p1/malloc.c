767bcd9a959a70f3c7f8fcfc91293784fd9cfddf
Viggó <viggoymir1@gmail.com>
2024-02-21 23:34:24 +0000

    heres to hoping

--- P5/p1/malloc.c
+++ P5/p1/malloc.c
@@ -166,6 +166,9 @@ static void __attribute__ ((unused)) merge_blocks(Block *block1, Block *block2)
 
 void my_free(void *address)
 {
+
+	(void) address;
+
 	// TODO: Implement
 
 	// !!
@@ -173,42 +176,38 @@ void my_free(void *address)
 
 	if (address == NULL) {
 		return;
-	} else {
-		Block *hptr = (Block *)ptr - 1;
-
 	}
-
 }
 
 
-// int main()
-// {
-// 	test_start("malloc.c");
-// 	initAllocator();
+int main()
+{
+	test_start("malloc.c");
+	initAllocator();
 
-// 	void *a = my_malloc(2016);
-// 	my_malloc(3);
-// 	my_malloc(28372);
-// 	void *b = my_malloc(16);
-// 	void *c = my_malloc(5);
-// 	my_malloc(2112);
+	void *a = my_malloc(2016);
+	my_malloc(3);
+	my_malloc(28372);
+	void *b = my_malloc(16);
+	void *c = my_malloc(5);
+	my_malloc(2112);
 
-// 	dumpAllocator();
+	dumpAllocator();
 
-// 	my_free(b);
-// 	my_free(c);
-// 	my_free(a);
-// 	dumpAllocator();
-// 	my_malloc(16746540);
-// 	my_malloc(1);
-// 	my_malloc(1);
-// 	my_malloc(1);
-// 	my_malloc(1);
-// 	dumpAllocator();
-// 	my_malloc(16775168);
-// 	my_malloc(1);
-// 	dumpAllocator();
+	my_free(b);
+	my_free(c);
+	my_free(a);
+	dumpAllocator();
+	my_malloc(16746540);
+	my_malloc(1);
+	my_malloc(1);
+	my_malloc(1);
+	my_malloc(1);
+	dumpAllocator();
+	my_malloc(16775168);
+	my_malloc(1);
+	dumpAllocator();
 	
-// 	printf("\nNote: This test always passes. It does not do any checks.\n It just prints the memory layout after some operations\n");
-// 	return test_end();
-// }
+	printf("\nNote: This test always passes. It does not do any checks.\n It just prints the memory layout after some operations\n");
+	return test_end();
+}
