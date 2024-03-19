d70e1cfa0ebdcceeedaf66f708eb8f99ce317e87
Brybbinnn <bryndisgunnarsd@gmail.com>
2024-03-19 19:57:42 +0000

    mdsmfsmmam

--- P9/p2/multi_mutex.c
+++ P9/p2/multi_mutex.c
@@ -2,20 +2,29 @@
 #include <unistd.h>
 #include <string.h>
 #include <alloca.h>
+#include <pthread.h>
 
 #include "multi_mutex.h"
 
 int multi_mutex_unlock(pthread_mutex_t **mutexv, int mutexc)
 {
-    (void) mutexv;
-    (void) mutexc;
+    for (int i = 0; i < mutexc; i++) {
+        if (pthread_mutex_unlock(mutexv[i]) != 0) {
+            return -1;
+        
+        };
+    }
     return 0;
 }
 
 int multi_mutex_trylock(pthread_mutex_t **mutexv, int mutexc)
 {
-    (void) mutexv;
-    (void) mutexc;
+    for (int i = 0; i < mutexc; i++) {
+        if (pthread_mutex_trylock(mutexv[i]) != 0) {
+            return -1;
+        
+        };
+    }
     return 0;
 }
 
